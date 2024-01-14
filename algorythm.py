cad341d5ed344e016fc5b7faf71c834233eafcb5
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2024-01-13 16:19:03 -0800

    algo of detecting palindromes in python

---
+++ algorythm.py
@@ -0,0 +1,11 @@
+def isPalindrome(str):
+    startIndex = 0
+    endIndex = len(str) - 1
+
+    for i in str:
+        if str[startIndex] != str[endIndex]:
+            return False
+        return True
+    
+
+print(isPalindrome('racecar'))
